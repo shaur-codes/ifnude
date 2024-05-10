d6bf804d23e6afe63bb83bfd3363cf1a96e9b160
Shaurya Mishra <168108708+shaur-codes@users.noreply.github.com>
2024-05-10 19:58:24 +0530

    Update detector.py
    added a new feature to censor video

--- ifnude/detector.py
+++ ifnude/detector.py
@@ -30,7 +30,6 @@ if not os.path.exists(model_folder):
 model_name = os.path.basename(model_url)
 model_path = os.path.join(model_folder, model_name)
 classes_path = os.path.join(model_folder, "classes")
-count=0
 
 if not os.path.exists(model_path):
     print("Downloading the detection model to", model_path)
@@ -98,18 +97,14 @@ def censor(img_path, out_path=None, visualize=False, parts_to_blur=[]):
         image = cv2.rectangle(
             image, (box[0], box[1]), (box[2], box[3]), (0, 0, 0), cv2.FILLED
         )
-    
-
 
     return image
 
 
-
-
-def extract_frames():
+def extract_frames(inp_video_path):
 
     global total_frames,fps,frame_width,frame_height,count
-
+    inp_video=inp_video = cv2.VideoCapture(inp_video_path)
     if not os.path.exists('temp'):
         try:
             os.makedirs('temp')
@@ -125,7 +120,7 @@ def extract_frames():
     frame_width = int(inp_video.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(inp_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     progress_bar=tqdm(total=total_frames,desc="Extracting frames",ncols=100)
-
+    count=0
     if inp_video.isOpened():
 
         while True:
@@ -140,46 +135,76 @@ def extract_frames():
         print(f"extracted {count} frames")
         inp_video.release()
 
-def censor_video(inp_video_path, out_video):
-    global total_frames, count, inp_video
-    inp_video = cv2.VideoCapture(inp_video_path)
-    out_video = out_video
-    
-    extract_frames()
-    
 
-    if total_frames is None:
-        print("Error: No frames extracted!!")
-        return
+def censor_video(inp_video_path, out_video_path, mode="default", min_prob=None, parts_to_blur=[]):
     
-    
-    detection_progress_bar = tqdm(total=total_frames, desc="detecting and censoring frames", ncols=100)
+    extract_frames(inp_video_path=inp_video_path)
+    p_bar=tqdm(total=total_frames,desc="detecting and censoring frames",ncols=100)
+    inp_video_path=inp_video_path
+    f = 0
 
     for i in range(total_frames):
+        detection_model = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+        img=os.path.join('temp',f'frame{f}.jpg')
+        if mode == "fast":
+            image, scale = preprocess_image(img, min_side=480, max_side=800)
+            if not min_prob:
+                min_prob = 0.5
+        else:
+            image, scale = preprocess_image(img)
+            if not min_prob:
+                min_prob = 0.6
+
+        outputs = detection_model.run(
+            [s_i.name for s_i in detection_model.get_outputs()],
+            {detection_model.get_inputs()[0].name: np.expand_dims(image, axis=0)},
+        )
+
+        labels = [op for op in outputs if op.dtype == "int32"][0]
+        scores = [op for op in outputs if isinstance(op[0][0], np.float32)][0]
+        boxes = [op for op in outputs if isinstance(op[0][0], np.ndarray)][0]
+
+        boxes /= scale
+        processed_boxes = []
+
         frame_path=os.path.join('temp','frame{:d}.jpg'.format(i))
-        print("frame path")
-        print(frame_path)
-        detected_boxes=detect(frame_path,min_prob=0.6)
-        print("detectted boxes")
-        print(detected_boxes)
-        for box in detected_boxes:
-            if box['score']>0.6:
-                print(box['score'],box['label'])
-                censored_image=censor(img_path=frame_path,out_path=out_video,parts_to_blur=[box['label'] for box in detected_boxes],visualize=False)
-                print("censored image")
-                print(censored_image)
-                if censored_image is None or censored_image.size==0 or censored_image.data is None:
-                    print(f"Error: censor function returned an empty image for frame {i}")
-                cv2.imwrite(frame_path,censored_image)
-            else:
+        
+        print('frame path',frame_path)
+        # Initiating Detection
+        for box, score, label in zip(boxes[0], scores[0], labels[0]):
+            if score < min_prob:
                 continue
+            box = box.astype(int).tolist()
+            label = classes[label]
+            if label == "EXPOSED_BELLY":
+                continue
+            processed_boxes.append(
+                {"box": [int(c) for c in box], "score": float(score), "label": label}
+            )
 
-        detection_progress_bar.update(1)
-    detection_progress_bar.close()
+        image=cv2.imread(frame_path)
+        boxes=processed_boxes
+        # Censoring the frame
+        if parts_to_blur:
+            boxes = [i["box"] for i in boxes if i["label"] in parts_to_blur]
+        else:
+            boxes = [i["box"] for i in boxes]
 
+        for box in boxes:
+            image = cv2.rectangle(
+                image, (box[0], box[1]), (box[2], box[3]), (0, 0, 0), cv2.FILLED
+            )
+
+        cv2.imwrite(frame_path,image)
+        p_bar.update(1)
+        f += 1
+
+    p_bar.close()
+
+    
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(out_video, fourcc, fps, (frame_width,frame_height))
+    out = cv2.VideoWriter(out_video_path, fourcc, fps, (frame_width,frame_height))
 
     for i in range(total_frames):
         frame_path=os.path.join('temp', 'frame{:d}.jpg'.format(i))
@@ -188,26 +213,14 @@ def censor_video(inp_video_path, out_video):
 
     out.release()
 
-    if out_video is not None:
-        print("file saved as {}".format(out_video))
+    if out_video_path is not None:
+        print("file saved as {}".format(out_video_path))
     else:
         print("Error: No output video file specified")
 
     
     print("cleaning temporary files")
     try:
-        os.rmdir('temp')
+        os.removedirs('temp')
     except Exception as e:
         return e
-
-
-
-
-
-
-
-
-
-
-
-
